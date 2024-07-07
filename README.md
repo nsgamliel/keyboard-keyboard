5365b047a5ecbc06c5489f6db3fd37e8242e1699
nsgamliel <nsgamliel@gmail.com>
2024-07-07 14:36:10 -0400

    add keypress animations, note names

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Keyboard Keyboard
 
 A mini keyboard designed to be played from your keyboard
+
+Note: The Keyboard Keyboard currently does not support mobile use, although this is in the works
--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const drawKeyboard = (keyboard) => {
 	canvas.width = WIDTH;
 	canvas.height = HEIGHT;
 
+	
+	const FONTSIZE = 16;
+	ctx.font = `${FONTSIZE}px Arial`;
 	ctx.strokeStyle = '#777';
 	ctx.lineWidth = 2;
 	const whiteKeyWidth = WIDTH / 8;
@@ -18,6 +21,11 @@ const drawKeyboard = (keyboard) => {
 			ctx.fillRect(whiteKeyWidth * i, 0, whiteKeyWidth, HEIGHT);
 		}
 		ctx.strokeRect(whiteKeyWidth * i, 0, whiteKeyWidth, HEIGHT);
+		ctx.fillStyle = '#000';
+		ctx.fillText(note.keyName,
+			whiteKeyWidth * i + (whiteKeyWidth / 2 - FONTSIZE/4),
+			HEIGHT - 1.5*FONTSIZE
+		);
 	}
 	
 	ctx.fillStyle = '#000';
@@ -28,11 +36,16 @@ const drawKeyboard = (keyboard) => {
 			var note;
 			Object.keys(keyboard).forEach(x => note = keyboard[x].bkID === i ? keyboard[x]: note);
 			if (note.pressed) {
-				ctx.fillStyle = '#444';
+				ctx.fillStyle = '#666';
 			}
 			ctx.fillRect(
 				(whiteKeyWidth * (i+1)) - (blackKeyWidth / 2), 0,
 				blackKeyWidth, blackKeyHeight
+			);
+			ctx.fillStyle = '#fff';
+			ctx.fillText(note.keyName,
+				(whiteKeyWidth * (i+1)) - FONTSIZE/2.75,
+				blackKeyHeight - FONTSIZE/1.5
 			);
 			ctx.fillStyle = '#000';
 		}
@@ -47,56 +60,103 @@ const g3 = new Audio("notes/G3.wav");
 const a3 = new Audio("notes/A3.wav");
 const b3 = new Audio("notes/B3.wav");
 const c4 = new Audio("notes/C4.wav");
+const cSh3 = new Audio("notes/Cs3.wav");
+const dSh3 = new Audio("notes/Ds3.wav");
+const fSh3 = new Audio("notes/Fs3.wav");
+const gSh3 = new Audio("notes/Gs3.wav");
+const bFl3 = new Audio("notes/Bb3.wav");
 
-// consider assigning value here to be object with audio elem and key to animate elem
 var notes = {
 	'a': {
 		audio: c3,
 		wkID: 0,
 		bkID: -1,
 		pressed: false,
+		keyName: 'A',
 	},
 	's': {
 		audio: d3,
 		wkID: 1,
 		bkID: -1,
 		pressed: false,
+		keyName: 'S',
 	},
 	'd': {
 		audio: e3,
 		wkID: 2,
 		bkID: -1,
 		pressed: false,
+		keyName: 'D',
 	},
 	'f': {
 		audio: f3,
 		wkID: 3,
 		bkID: -1,
 		pressed: false,
+		keyName: 'F',
 	},
 	'g': {
 		audio: g3,
 		wkID: 4,
 		bkID: -1,
 		pressed: false,
+		keyName: 'G',
 	},
 	'h': {
 		audio: a3,
 		wkID: 5,
 		bkID: -1,
 		pressed: false,
+		keyName: 'H',
 	},
 	'j': {
 		audio: b3,
 		wkID: 6,
 		bkID: -1,
 		pressed: false,
+		keyName: 'J',
 	},
 	'k': {
 		audio: c4,
 		wkID: 7,
 		bkID: -1,
 		pressed: false,
+		keyName: 'K',
+	},
+	'w': {
+		audio: cSh3,
+		wkID: -1,
+		bkID: 0,
+		pressed: false,
+		keyName: 'W',
+	},
+	'e': {
+		audio: dSh3,
+		wkID: -1,
+		bkID: 1,
+		pressed: false,
+		keyName: 'E',
+	},
+	't': {
+		audio: fSh3,
+		wkID: -1,
+		bkID: 3,
+		pressed: false,
+		keyName: 'T',
+	},
+	'y': {
+		audio: gSh3,
+		wkID: -1,
+		bkID: 4,
+		pressed: false,
+		keyName: 'Y',
+	},
+	'u': {
+		audio: bFl3,
+		wkID: -1,
+		bkID: 5,
+		pressed: false,
+		keyName: 'U',
 	},
 };
 
